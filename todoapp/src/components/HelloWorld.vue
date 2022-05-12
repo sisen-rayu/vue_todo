c09f7d11cb27a19f453e712b871bcd7857cf59b8
chihiro.tanigawa <chihiro.tanigawa@accenture.com>
2022-05-12 15:15:24 +0900

    リストの項目を削除する(sessionからも削除)

--- todoapp/src/components/HelloWorld.vue
+++ todoapp/src/components/HelloWorld.vue
@@ -2,6 +2,7 @@
   <div class="hello">
     <!-- <h1>{{ msg }}</h1> -->
     <h2>Todo app</h2>
+      <button style="submit" @click="deleteAll(todo)">DeleteALL</button>
     <h3>Enter todo</h3>
     <p>Name:</p>
     <input v-model="todo.name">
@@ -13,10 +14,12 @@
     <button style="submit" @click="create">Create</button>
     <hr>
 
+    <button style="submit" @click="read">Read</button>
     <div v-for="todo in todos" :key="todo.name">
       <li>Name:{{ todo.name }}</li>
       <li>Description:{{ todo.description }}</li>
       <li>Deadline:{{ todo.deadline }}</li>
+      <button style="submit" @click="deleteTodo(todo)">Delete</button>
       <hr>
     </div>
   </div>
@@ -27,8 +30,8 @@ export default {
   name: 'HelloWorld',
   data: () => ({
     todos: [
-      { name: "walking", description: "walking", deadline:"0401"},
-      { name: "walking", description: "walking", deadline:"0401"}
+      // { name: "walking", description: "walking", deadline:"0401"},
+      // { name: "walking", description: "walking", deadline:"0401"}
     ],
     todo: {
       name: "",
@@ -38,6 +41,12 @@ export default {
     name: "todos"
   }),
   methods: {
+    read() {
+      const data = sessionStorage.getItem(this.name)
+      if (data) {
+        this.todos = JSON.parse(data)
+      }
+    },
     create() {
       // this.name = sessionStorage.getItem("name")
       this.todos.push(this.todo)
@@ -47,6 +56,15 @@ export default {
         description: "",
         deadline: ""
       }
+    },
+    deleteTodo(todo) {
+      this.todos = this.todos.filter((item) => item.name !== todo.name)
+      sessionStorage.setItem(this.name, JSON.stringify(this.todos))
+    },
+    deleteAll() {
+      // sessionstorageの中身を空にするメソッド
+      sessionStorage.clear()
+      this.todos = []
     }
   }
 }
